2cfc97a31d53f668288a4b7d6d0cb19b71b822d5
Matei Ionita <matei.ionita@gmail.com>
2024-06-06 16:49:04 -0400

    added option to limit cores for parallelization

--- main_parallel.R
+++ main_parallel.R
@@ -4,6 +4,7 @@ library(parallel)
 
 dir_in <- Sys.getenv("INPUT_DIR")
 dir_out <- Sys.getenv("OUTPUT_DIR")
+max_cores <- Sys.getenv("MAX_CORES")
 
 files <- list.files(dir_in, pattern=".fcs")
 
@@ -18,6 +19,7 @@ create_dirs(dir_out)
 
 ### run analysis in parallel
 nc <- detectCores()
+nc <- min(nc, max_cores)
 cl <- makeCluster(nc)
 
 clusterExport(cl, c("dir_in", "dir_out", "cols"))
